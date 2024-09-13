293a79858adbd770c50382e2f8b7470b416ffb58
Yousuke7052 <130848000+Yousuke7052@users.noreply.github.com>
2024-09-14 00:18:37 +0800

    Update deploy.py
    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -114,10 +114,10 @@ def handle_branch_logic():
 
     # 处理文件
     if branch_name == 'dev':
-        file_path = 'dev.html'
+        file_path = './dev.html'
         destination_path = 'dev.html'
     elif branch_name == 'prod':
-        file_path = 'prod.html'
+        file_path = './prod.html'
         destination_path = 'prod.html'
 
     handle_file(branch_name, file_path, destination_path)
