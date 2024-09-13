3d6c8a5c720c0b3d466267b910fdcc3b92c97749
Yousuke7052 <130848000+Yousuke7052@users.noreply.github.com>
2024-09-14 02:55:42 +0800

    Update dev.py
    Update dev.py

--- dev.py
+++ dev.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import sys
 
 # OSS相关环境变量
 OSS_ACCESS_KEY_ID = os.getenv('OSS_ACCESS_KEY_ID')
@@ -13,10 +14,11 @@ def get_current_branch():
         branch_name = subprocess.check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD']).strip()
         return branch_name.decode('utf-8')
     except subprocess.CalledProcessError as e:
-        log('Failed to get current branch: %s' % str(e))
+        print('Failed to get current branch: %s' % str(e))
         return None
 
 def upload_file_to_oss(file_path, destination_path):
+    """上传文件到OSS"""
     try:
         # 使用旧版字符串格式化方法
         oss_url = 'oss://%s/%s' % (OSS_BUCKET_NAME, destination_path)
@@ -33,12 +35,19 @@ def upload_file_to_oss(file_path, destination_path):
         print("Failed to upload file %s: %s" % (file_path, e))
 
 if __name__ == '__main__':
-    # 假设你要上传的是名为hello.txt的文件
-    file_path = 'dev.html'  # 相对路径
-    destination_path = 'dev.html'  # OSS中的目标路径
+    # 获取当前分支名
+    branch_name = get_current_branch()
     
-    # 检查文件是否存在
-    if os.path.exists(file_path):
-        upload_file_to_oss(file_path, destination_path)
+    if branch_name == 'dev':
+        # 假设你要上传的是名为hello.txt的文件
+        file_path = 'dev.html'  # 相对路径
+        destination_path = 'dev.html'  # OSS中的目标路径
+        
+        # 检查文件是否存在
+        if os.path.exists(file_path):
+            upload_file_to_oss(file_path, destination_path)
+        else:
+            print("File %s does not exist." % file_path)
     else:
-        print("File %s does not exist." % file_path)
+        print("不是dev分支提交")
+        sys.exit(0)  # 结束脚本执行
