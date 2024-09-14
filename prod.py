5b34fa9f369c55f0b3d5d4139db966388a242b15
littlesun <liohahaku@gmail.com>
2024-09-14 15:29:17 +0800

    update

--- prod.py
+++ prod.py
@@ -9,35 +9,29 @@ OSS_ENDPOINT = os.getenv('OSS_ENDPOINT_prod')
 OSS_BUCKET_NAME = os.getenv('OSS_BUCKET_NAME_prod')
 
 def get_current_branch():
-    """获取当前分支名"""
+    """嘗試获取当前分支名"""
     try:
         branch_name = subprocess.check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD']).strip()
         return branch_name.decode('utf-8')
     except subprocess.CalledProcessError as e:
-        print('Failed to get current branch: %s' % str(e))
+        print('Failed to get current branch / 嘗試獲取prod分支失敗了: %s' % str(e))
         return None
 
 def check_file_changed(file_path):
     """检查特定文件是否在最近的提交中有更改"""
     try:
-        # 行 27
         last_commit_hash = subprocess.check_output(['git', 'rev-parse', 'HEAD~1']).strip().decode('utf-8')
-        # 行 30
         current_commit_hash = subprocess.check_output(['git', 'rev-parse', 'HEAD']).strip().decode('utf-8')
-        
-        # 行 34
         diff_output = subprocess.check_output(['git', 'diff', '--name-only', last_commit_hash, current_commit_hash, file_path])
         return bool(diff_output.strip())
     except subprocess.CalledProcessError as e:
-        print('Failed to check file changes: %s' % str(e))
+        print('Failed to check file changes / prod分支本次無提交oss靜態網站更新需求: %s' % str(e))
         return False
 
 def upload_file_to_oss(file_path, destination_path):
     """上传文件到OSS"""
     try:
-        # 使用旧版字符串格式化方法
         oss_url = 'oss://%s/%s' % (OSS_BUCKET_NAME, destination_path)
-        
         subprocess.run([
             'ossutil', 'cp', file_path,
             oss_url,
@@ -56,14 +50,15 @@ if __name__ == '__main__':
     
     if branch_name == 'prod':
         # 假设你要上传的是名为hello.txt的文件
-        file_path = 'prod.html'  # 相对路径
-        destination_path = 'prod.html'  # OSS中的目标路径
+        file_path = 'prod.html'  # DevOps中會在同級目錄下
+        destination_path = 'prod.html'  # OSS中的暫定目標路徑
         
-        # 检查文件是否存在
+        # 检查文件是否更新過
         if check_file_changed(file_path):
+            # 上傳更新的文件至oss
             upload_file_to_oss(file_path, destination_path)
         else:
-            print("File %s does not exist." % file_path)
+            print("File %s does not exist. / %s 未檢測到更新" % (file_path,file_path))
     else:
-        print("不是dev分支提交")
+        print(" %s 分支並未做提交" % file_path)
         sys.exit(0)  # 结束脚本执行
